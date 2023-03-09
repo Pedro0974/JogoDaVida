617842417545634c45fba16b07bdf679b6266737
Pedro <111787020+Pedro0974@users.noreply.github.com>
2023-03-09 20:25:16 -0300

    finalizando trabalho

--- src/edu/pedroMayconVictorA/trabalho/Main.java
+++ src/edu/pedroMayconVictorA/trabalho/Main.java
@@ -6,24 +6,27 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		System.out.print("Digite o tamanho do tabuleiro: ");
+		//le o o numero
 		int size = scan.nextInt();
+		scan.nextLine(); // consumir a quebra de linha deixada pelo nextInt()
 		JogoDaVida game = new JogoDaVida(size);
 		if (size > 1) {
 			game.initialize();
 			game.display();
+
 			while (true) {
-				//test
 				String input = scan.nextLine();
+
 				if (input.equalsIgnoreCase("s")) {
 					game.display();
 					game.update();
-				}
-				if (input.equalsIgnoreCase("n")) {
+				} else if (input.equalsIgnoreCase("n")) {
 					System.out.println("Você Encerrou Sua Vida!");
 					break;
+				} else {
+					System.out.println("Opção inválida. Digite S ou N.");
 				}
 			}
-
 		}
 	}
 }
